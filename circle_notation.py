ee9862835557b646c0cc77bfbcf8f3b7ae99c960
SergioJF10 <Sergio.Jimenez19@alu.uclm.es>
2023-11-23 17:35:47 +0100

    Amplitude rotation counterclockwise fix

--- circle_notation.py
+++ circle_notation.py
@@ -18,9 +18,11 @@ def get_amplitudes(sv):
     '''Extract and normalize the data structure with the amplitudes'''
     sv_dict = sv.to_dict()
 
+    # Standardization I (include all amplitudes)
     if '0' not in sv_dict.keys(): sv_dict['0'] = 0.0
     if '1' not in sv_dict.keys(): sv_dict['1'] = 0.0
-    
+
+    # Standardization II (first |0> then |1>)
     sorted_dict = {}
     for key in sorted(sv_dict.keys()):
         sorted_dict[key] = sv_dict[key]
@@ -31,25 +33,26 @@ def get_amplitudes(sv):
 
 def plot_initial_circles(ax, padding):
     '''Plotting the empty white initial circles per each component'''
-    # Calculamos el radio de las circunferencias iniciales
+    # Initial circles radius calculation
     initial_radius = 1 / math.sqrt(math.pi)
     
-    # Calculamos los centros de las circunferencias iniciales
+    # Initial circles centers calculation
     centers = ((0, 0), ((2 * initial_radius) + padding, 0))
 
-    # Dibujamos el círculo del componente |0>
+    # Compontent |0> circle plotting
     circulo_0 = plt.Circle(centers[0], initial_radius, color='black', fill=False)
     ax.add_patch(circulo_0)
 
-    # Dibujamos el círculo del componente |1>
+    # Compontent |1> circle plotting
     circulo_1 = plt.Circle(centers[1], initial_radius, color='black', fill=False)
     ax.add_patch(circulo_1)
 
-    # Dibujamos las labels
+    # Plot labels
     ax.text(0, -1.5 * initial_radius, '|0>', ha='center', va='center')
     ax.text((2 * initial_radius) + 0.25, -1.5 * initial_radius, '|1>', ha='center', va='center')
-    
-    return centers, initial_radius # Devolvemos los centros y el radio porque nos serán útiles después
+
+    # Return centers and initial radius for later computations
+    return centers, initial_radius
 
 def pick_normalized_color(phase):
     '''Get a color corresponding to the given phase'''
@@ -57,45 +60,39 @@ def pick_normalized_color(phase):
     custom_cmap = plt.get_cmap('hsv')
     return clr.to_hex(custom_cmap(norm(phase)))
 
-def plot_polar_colorbase():
-    '''Plot a colorbase for showing the phase-color mapping'''
-    display_axes = fig.add_axes([0.1,0.1,0.8,0.8], projection='polar') # polar es para que sea en círculo
-    norm = clr.Normalize(0.0, 2*math.pi)
-    cb = clb.ColorbarBase(display_axes, cmap=plt.get_cmap('hsv'), norm=norm, orientation='horizontal')
-
-    # no box grid
-    cb.outline.set_visible(False)
-    # plot labels in radians
-    ticks_position = plt.xticks()[0]
-    radian_ticks = xL=['0',r'$\frac{\pi}{4}$',r'$\frac{\pi}{2}$',r'$\frac{3\pi}{4}$',\
-            r'$\pi$',r'$\frac{5\pi}{4}$',r'$\frac{3\pi}{2}$',r'$\frac{7\pi}{4}$']
-    plt.xticks(ticks_position, radian_ticks)
-    # inner white ring
-    display_axes.set_ylim(-2,1)
-    plt.show()
-
 def plot_inner_circles(ax, centers, amplitudes, relative_phase):
     '''Plotting the inner circles with area propotional to the amplitude'''
-    # Calcular los radios de los círculos internos
+    # Inner circles radius calculation
     inner_radius = (abs(amplitudes[0]) / math.sqrt(math.pi), abs(amplitudes[1]) / math.sqrt(math.pi))
 
-    # Dibujamos el círculo interno del componente |0> en el centro correspondiente
+    # Compontent |0> inner circle plotting in adequate center
     circulo_0 = plt.Circle(centers[0], inner_radius[0], color=pick_normalized_color(0))
     ax.add_patch(circulo_0)
 
-    # Dibujamos el círculo interno del componente |1> en el centro correspondiente
+    # Compontent |1> inner circle plotting in adequate center
     circulo_1 = plt.Circle(centers[1], inner_radius[1], color=pick_normalized_color(relative_phase))
     ax.add_patch(circulo_1)
 
+def get_relative_phase(amplitudes):
+    '''Calculates and standardices the relative phase for the amplitudes'''
+    # Previous reasoning computation
+    relative_phase = cmath.phase(amplitudes[1]) - cmath.phase(amplitudes[0])
+
+    # Counterclockwise angle rotation
+    if relative_phase < 0: 
+        relative_phase += (2 * math.pi)
+
+    return relative_phase
+
 def plot_radius(ax, centers, amplitudes, initial_radius):
     '''Plotting both radios representing the relative phase'''
-    # Calculamos la fase relativa
-    relative_phase = abs(cmath.phase(amplitudes[0]) - cmath.phase(amplitudes[1]))
+    # Calculate relative phase
+    relative_phase = get_relative_phase(amplitudes)
     
-    # Dibujamos el radio del componente |0>
+    # Component |0> radius plotting
     ax.plot([centers[0][0], centers[0][0] + initial_radius], [centers[0][1], centers[0][1]], color='black')
     
-    # Dibujamos el radio del componente |1>
+    # Component |1> radius plotting
     a = centers[1][0] + (initial_radius * math.cos(relative_phase))
     b = centers[1][1] + (initial_radius * math.sin(relative_phase))
     ax.plot([centers[1][0], a], [centers[1][1], b], color='black')
@@ -116,18 +113,18 @@ def prepare_statevector():
 
 def circle_notation(sv):
     '''Main method with the final algorithm for plotting'''
-    # Configuramos la figura
+    # Set the figure
     fig, ax = plt.subplots(figsize=(3,1.5))
     ax.set_axis_off()
     padding = 0.25
 
-    # Pasos principales
+    # Main steps
     amplitudes = get_amplitudes(sv)
     centers, initial_radius = plot_initial_circles(ax, padding)
     relative_phase = plot_radius(ax, centers, amplitudes, initial_radius)
     plot_inner_circles(ax, centers, amplitudes, relative_phase)
 
-    # Ajustamos los límites de la figura a los radios y el relleno entre círculos (padding)
+    # Adjust figure limits and circles padding
     ax.set_xlim(-(initial_radius) , 3 * initial_radius + padding)
     ax.set_ylim(-initial_radius, initial_radius)
     plt.show()
